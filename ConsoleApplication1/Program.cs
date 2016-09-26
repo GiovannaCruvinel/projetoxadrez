b7cc813caaa4045ed985a110e60797e89b81c57b
Giovanna Chiba <gigichiba@gmail.com>
2016-09-26 09:21:46 -0300

    Primeiro commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um valor inteiro: ");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+
+            Console.WriteLine("DOBRO = " + y);
+
+            Console.ReadLine();
+
+        }
+    }
+}
